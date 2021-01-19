f44230577c88d7db17a2c56a4c0734b33e37cac0
luisflv149808 <39777455+luisflv149808@users.noreply.github.com>
2021-01-19 09:05:10 -0700

    Update main.dart

--- arabigo_maya/lib/main.dart
+++ arabigo_maya/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
-        title: 'Atinale Al Numero',
+        title: 'Conversor',
         theme: ThemeData(
           brightness: Brightness.light,
           primaryColor: Colors.purple[600],
